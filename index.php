192972cf62ec8a0da627d4c70843873c3af4a8e3
albertoosr <juan.alberto081099@gmail.com>
2020-09-12 17:30:19 -0500

    project alfa

---
+++ index.php
@@ -0,0 +1,391 @@
+<?php require('views/layouts/header.php'); ?>
+
+<nav class="navbar navbar-main navbar-expand-lg navbar-light border-bottom">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="main_nav">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link pl-0" data-toggle="dropdown" href="#"><strong> <i class="fa fa-bars"></i> &nbsp All Category</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fashion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Supermarket</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Electronics</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Baby & Toys</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fitness sport</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Clothing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Furnitures</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<section class="section-intro padding-y-sm">
+    <div class="container">
+        <div class="intro-banner-wrap">
+            <img src="resources/img/banner.jpg" class="img-fluid rounded">
+        </div>
+    </div>
+</section>
+
+
+<section class="section-content padding-y-sm">
+    <div class="container">
+        <article class="card card-body">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <figure class="item-feature">
+                        <span class="text-primary"><i class="fa fa-2x fa-truck"></i></span>
+                        <figcaption class="pt-3">
+                            <h5 class="title">Fast Delivery</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-4">
+                    <figure class="item-feature">
+                        <span class="text-primary"><i class="fa fa-2x fa-landmark"></i></span>
+                        <figcaption class="pt-3">
+                            <h5 class="title">Creative Strategy</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-4">
+                    <figure class="item-feature">
+                        <span class="text-primary"><i class="fa fa-2x fa-lock"></i></span>
+                        <figcaption class="pt-3">
+                            <h5 class="title">High Secured </h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                            </p>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </article>
+
+    </div>
+</section>
+
+
+<section class="section-content">
+    <div class="container">
+        <header class="section-heading">
+            <h3 class="section-title">Popular Products</h3>
+        </header>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img1.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just another product name</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img2.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Some item name here</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$280.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img3.jpg" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Great product name here</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$56.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img4.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just another product name</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="section-content">
+    <div class="container">
+
+        <header class="section-heading">
+            <h3 class="section-title">New Arrived</h3>
+        </header>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img5.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just Another Product Name</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img6.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Some Item Name Here</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$280.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img7.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Great Product Name Here</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$56.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img8.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just Another Product Name</a>
+
+                        <div class="rating-wrap">
+                            <ul class="rating-stars">
+                                <li style="width:80%" class="stars-active">
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                                <li>
+                                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                                </li>
+                            </ul>
+                            <span class="label-rating text-muted">34 reviws</span>
+                        </div>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="section-content padding-bottom-sm">
+    <div class="container">
+
+        <header class="section-heading">
+            <a href="#" class="btn btn-outline-primary float-right">See All</a>
+            <h3 class="section-title">Recommended</h3>
+        </header>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img7.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just Another Product Name</a>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img4.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Some Item Name Here</a>
+                        <div class="price mt-1">$280.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img2.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Great Product Name Here</a>
+                        <div class="price mt-1">$56.00</div>
+                    </figcaption>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div href="#" class="card card-product-grid">
+                    <a href="#" class="img-wrap"> <img src="resources/img/img5.png" style="max-width: 100%;"> </a>
+                    <figcaption class="info-wrap">
+                        <a href="#" class="title">Just Another Product Name</a>
+                        <div class="price mt-1">$179.00</div>
+                    </figcaption>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="section-name bg padding-y-sm">
+    <div class="container">
+        <header class="section-heading">
+            <h3 class="section-title">Our Brands</h3>
+        </header>
+
+        <div class="row">
+            <div class="col-md-2 col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/microsoft.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">36 Products</figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2  col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/amazon1.0.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">980 Products</figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2  col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/oracle.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">25 Products</figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2  col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/microsoft.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">72 Products</figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2  col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/oracle.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">41 Products</figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2  col-6">
+                <figure class="box item-logo">
+                    <a href="#"><img src="resources/img/amazon1.0.png" style="max-width: 100%;"></a>
+                    <figcaption class="border-top pt-2">12 Products</figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section-name padding-y">
+    <div class="container">
+        <h3 class="mb-3">Download App Demo Text</h3>
+        <a href="#"><img src="resources/img/download.png" height="45"></a>
+        <a href="#"><img src="resources/img/download.png" height="45"></a>
+
+    </div>
+</section>
+
+<?php require('views/layouts/footer.php'); ?>
---
+++ views/layouts/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+    <link rel="icon" type="image/png" href="logo.ico"/>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css" type="text/css">
+    <script src="https://kit.fontawesome.com/39610b5df0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="public/css/estilos.css">
+</head>
+<body>
+    <header class="section-header">
+
+        <nav class="navbar navbar-dark navbar-expand p-0 bg-primary">
+            <div class="container">
+                <ul class="navbar-nav d-none d-md-flex mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Delivery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Payment</a></li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="#" class="nav-link"> Call: +Example </a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"> English </a>
+                        <ul class="dropdown-menu dropdown-menu-right" style="max-width: 100px;">
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+
+        <section class="header-main border-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-2 col-6">
+                        <a href="#" class="brand-wrap">
+                            <img class="logo" src="resources/img/logo.png">
+                        </a>
+                    </div>
+
+                    <div class="col-lg-6 col-12 col-sm-12">
+                        <form action="#" class="search">
+                            <div class="input-group w-100">
+                                <input type="text" class="form-control" placeholder="Search">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="col-lg-4 col-sm-6 col-12">
+                        <div class="widgets-wrap float-md-right">
+                            <div class="widget-header  mr-3">
+                                <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+                                <span class="badge badge-pill badge-danger notify">2</span>
+                            </div>
+
+                            <div class="widget-header icontext">
+                                <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
+                                <div class="text">
+                                    <span class="text-muted">Welcome</span>
+                                    <div>
+                                        <a href="#">Sign in</a> |
+                                        <a href="#"> Register</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </header>
+</body>
+
+</html>
